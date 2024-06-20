1956be64f7c5a3d5d5ea43f8ca12394b6081a4a9
Eloi Stree <eloistree@gmail.com>
2024-06-20 19:59:46 +0200

    Save: 06/20/2024 7:59 PM

--- Runtime/FollowDroneTransformLerpMono.cs
+++ Runtime/FollowDroneTransformLerpMono.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowDroneTransformLerpMono : MonoBehaviour
 {
     public Transform m_target;
     public Transform m_whatToMove;
-    public float m_lerpSpeed = 0.1f;
+    public float m_lerpSpeedMove = 0.2f;
+    public float m_lerpSpeedRotate = 0.65f;
     void Update()
     {
         if (m_target != null && m_whatToMove != null)
         {
-            m_whatToMove.position = Vector3.Lerp(m_whatToMove.position, m_target.position, m_lerpSpeed);
-            m_whatToMove.rotation = Quaternion.Lerp(m_whatToMove.rotation, m_target.rotation, m_lerpSpeed);
+            m_whatToMove.position = Vector3.Lerp(m_whatToMove.position, m_target.position, m_lerpSpeedMove);
+            m_whatToMove.rotation = Quaternion.Lerp(m_whatToMove.rotation, m_target.rotation, m_lerpSpeedRotate);
         }
     }
 }

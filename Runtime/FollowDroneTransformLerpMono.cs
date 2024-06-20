41bc6a8279cec6e5ec37677a5148db008b14745b
Eloi Stree <eloistree@gmail.com>
2024-06-20 20:40:55 +0200

    Save: 06/20/2024 8:40 PM

--- Runtime/FollowDroneTransformLerpMono.cs
+++ Runtime/FollowDroneTransformLerpMono.cs
@@ -6,7 +6,20 @@ public class FollowDroneTransformLerpMono : MonoBehaviour
     public Transform m_whatToMove;
     public float m_lerpSpeedMove = 0.2f;
     public float m_lerpSpeedRotate = 0.65f;
+
+    public bool m_useLateUpdate;
     void Update()
+    {
+        if (!m_useLateUpdate)
+            UpdateLerp();
+    }
+    void LateUpdate()
+    {
+        if (m_useLateUpdate)
+            UpdateLerp();
+    }
+
+    private void UpdateLerp()
     {
         if (m_target != null && m_whatToMove != null)
         {

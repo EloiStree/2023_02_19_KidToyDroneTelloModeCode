d037528fd20dc4604d30be3349837d1a4bc06aee
Eloi Strée <eloistree@gmail.com>
2024-10-16 16:45:27 +0200

    Save: 10/16/2024 4:45 PM

--- Runtime/Script/RCEasyMoveControllerLerpMono.cs
+++ Runtime/Script/RCEasyMoveControllerLerpMono.cs
@@ -8,9 +8,7 @@ public class RCEasyMoveControllerLerpMono : MonoBehaviour
 {
 
     public Vector3 m_distanceInMeterPerSeconds = new Vector3(0.5f, 0.2f, 0.8f);
-    public Vector3 m_speedPerDirectionPercent = new Vector3(0.3f,0.3f,0.3f);
     public float m_horizontalAnglePerSeconds=90;
-    public float m_rotationSpeedPercent = 0.3f;
 
     public Transform m_rootOfCurrentObject;
 
@@ -33,12 +31,9 @@ public class RCEasyMoveControllerLerpMono : MonoBehaviour
         Vector3 droneRot = m_rootOfCurrentObject.rotation.eulerAngles;
         droneRot.x = 0;
         Quaternion r = Quaternion.Euler(droneRot);
-        move += Vector3.forward * m_distanceInMeterPerSeconds.z * dt * m_directionUserPrevious.z 
-            * m_speedPerDirectionPercent.z;
-        move += Vector3.up * m_distanceInMeterPerSeconds.y * dt * m_directionUserPrevious.y
-            * m_speedPerDirectionPercent.y;
-        move += Vector3.right * m_distanceInMeterPerSeconds.x * dt * m_directionUserPrevious.x
-            * m_speedPerDirectionPercent.x;
+        move += Vector3.forward * m_distanceInMeterPerSeconds.z * dt * m_directionUserPrevious.z ;
+        move += Vector3.up * m_distanceInMeterPerSeconds.y * dt * m_directionUserPrevious.y;
+        move += Vector3.right * m_distanceInMeterPerSeconds.x * dt * m_directionUserPrevious.x;
         move = r * move;
 
         m_rootOfCurrentObject.position += move;
@@ -46,7 +41,7 @@ public class RCEasyMoveControllerLerpMono : MonoBehaviour
 
         m_rootOfCurrentObject.rotation = m_rootOfCurrentObject.rotation *
             Quaternion.Euler(0, m_horizontalAnglePerSeconds
-            *dt* m_rotateLeftRightUserPrevious * m_rotationSpeedPercent, 0);
+            *dt* m_rotateLeftRightUserPrevious, 0);
     }
     public void SetFrontalMove(float backForward)
     {
@@ -68,25 +63,7 @@ public class RCEasyMoveControllerLerpMono : MonoBehaviour
         m_directionUser.y = Mathf.Clamp(downUp,-1,1);
     }
 
-    public void SetFrontalMoveSpeedPercent(float backForward)
-    {
-        m_speedPerDirectionPercent.z = Mathf.Clamp(backForward, -1, 1);
-    }
-
-    public void SetHorizontalRotationSpeedPercent(float rotateLeftRight)
-    {
-        m_rotationSpeedPercent = Mathf.Clamp(rotateLeftRight, -1, 1);
-    }
-
-    public void SetHorizontaMoveSpeedPercent(float leftRight)
-    {
-        m_speedPerDirectionPercent.x = Mathf.Clamp(leftRight, -1, 1);
-    }
-
-    public void SetVerticalMoveSpeedPercent(float downUp)
-    {
-        m_speedPerDirectionPercent.y = Mathf.Clamp(downUp, -1, 1);
-    }
+   
 
 
 }
